88b23a2f84d7c1cd54a188744b00ff01c1353d67
luozhiran <820152235@qq.com>
2020-05-14 20:48:18 +0800

    d

--- src/services/ServletDemo.java
+++ src/services/ServletDemo.java
@@ -5,12 +5,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
 
 public class ServletDemo extends HttpServlet {
+
+    private String message;
 
     @Override
     public void init() throws ServletException {
         super.init();
+        message = "Hello word!!!!!!!!!!!";
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -19,5 +24,15 @@ public class ServletDemo extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+//        response.setContentType("text/Html");
+        PrintWriter printWriter = response.getWriter();
+        printWriter.write(message);
+        printWriter.flush();
+        printWriter.close();
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
     }
 }
